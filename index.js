e2fc8cccb076fb55fa7dda36100a1e28305ee404
vignesh015 <vigneshrathnams@gmail.com>
2020-02-23 15:51:35 +0530

    Initial commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express=require("express");
 const MongoClient=require("mongodb").MongoClient;
 const ejs=require("ejs");
 const uri=require("./config/db");
-const client=new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
+const client=new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true });  
+const ejsLayouts=require("express-ejs-layouts");
 
 //Connecting to Database
 client.connect(err=>{
@@ -14,10 +15,19 @@ client.connect(err=>{
 //Express Application
 const app=express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello world");
+//View engine
+app.set("view engine","ejs");
+
+//Ejs layout setup
+app.use(ejsLayouts);
+
+//Routes
+app.get("*",(req,res)=>{
+    res.locals.index=req.url;
+    res.render("index");
 });
 
+//Accesing on port 3000
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
 });
