7fdec6c88906ca1cca4afb2f192b802454819770
vignesh015 <vigneshrathnams@gmail.com>
2020-02-23 13:29:20 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,3 @@
+const uri="mongodb+srv://vicky:vicky@123@cluster0-2fh28.mongodb.net/test?retryWrites=true&w=majority";
+
+module.exports=uri;
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+//Initializing and Declaring dependancies
 const express=require("express");
-const mongodb=require("mongodb");
+const MongoClient=require("mongodb").MongoClient;
 const ejs=require("ejs");
+const uri=require("./config/db");
+const client=new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
 
+//Connecting to Database
+client.connect(err=>{
+    if(err) throw err;
+    console.log("MongoDB connected");
+})
+
+//Express Application
 const app=express();
 
 app.get("/",(req,res)=>{
@@ -10,4 +20,4 @@ app.get("/",(req,res)=>{
 
 app.listen(3000,()=>{
     console.log("Server started at port 3000");
-})
+});
