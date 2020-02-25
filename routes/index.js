a61bbc32e43d503614aba1fbaf1d87984a835218
vignesh015 <vigneshrathnams@gmail.com>
2020-02-25 20:57:06 +0530

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express=require("express");
 const router=express.Router;
 
-router.get("/",(req,res)=>{
+router.get("/",(_req,res)=>{
     res.render("index");
-})
+});
 
-router.get("/signup",(req,res)=>{
+router.get("/signup",(_req,res)=>{
     res.render("signup");
-})
+});
 
-router.get("/login",(req,res)=>{
+router.get("/login",(_req,res)=>{
     res.render("login");
-})
+});
 
 module.exports=router;
